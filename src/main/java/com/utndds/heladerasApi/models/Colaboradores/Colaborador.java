5d554e1bba03c78333724f6e4c4a98ff5fe426d7
Diego-Ing <dyanezromero@frba.utn.edu.ar>
2024-05-03 10:19:49 -0300

    contacto arreglado

--- src/main/java/com/utndds/heladerasApi/models/Colaboradores/Colaborador.java
+++ src/main/java/com/utndds/heladerasApi/models/Colaboradores/Colaborador.java
@@ -2,7 +2,7 @@ package com.utndds.heladerasApi.models.Colaboradores;
 
 import java.util.ArrayList;
 import java.util.List; // Add this import statement
-import com.utndds.heladerasApi.models.Contacto; // Add this import statement
+import com.utndds.heladerasApi.models.Colaboradores.Contacto; // Add this import statement
 
 public class Colaborador {
 
---
+++ src/main/java/com/utndds/heladerasApi/models/Colaboradores/Contacto.java
@@ -0,0 +1,11 @@
+package com.utndds.heladerasApi.models.Colaboradores;
+
+public class Contacto {
+    String tipo;
+    String valor;
+
+    public Contacto(String tipo, String valor) {
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+}
