c1561634fd3ea93508763fae41b45cd9581b1c94
LautaroRomero21 <lautaroromerog021@gmail.com>
2024-07-15 12:20:51 -0300

    visita agregada

---
+++ src/main/java/com/utndds/heladerasApi/models/Visita.java
@@ -0,0 +1,33 @@
+package com.utndds.heladerasApi.models;
+
+import java.time.LocalDate;
+
+import com.google.protobuf.compiler.PluginProtos.CodeGeneratorResponse.File;
+import com.utndds.heladerasApi.models.Heladera.Heladera;
+import com.utndds.heladerasApi.models.Rol.Tecnico;
+
+public class Visita {
+    LocalDate fecha;
+    Tecnico tecnico;
+    Heladera heladera;
+    String descripcion;
+    File foto;
+    boolean arreglado;
+
+    public Visita(LocalDate fecha, Tecnico tecnico, Heladera heladera, String descripcion, File foto,
+            boolean arreglado) {
+        this.fecha = fecha;
+        this.tecnico = tecnico;
+        this.heladera = heladera;
+        this.descripcion = descripcion;
+        this.foto = foto;
+        this.arreglado = arreglado;
+
+        this.verificarArreglo();
+
+    }
+
+    private void verificarArreglo() {
+        System.out.println("se verifica el arreglo por parte del tecnico, sino se arregla se plantea otra visita");
+    }
+}
