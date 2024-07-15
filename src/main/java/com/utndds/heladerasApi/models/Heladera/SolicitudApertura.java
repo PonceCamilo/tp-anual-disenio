0f976a96425ac97488a85ba51466e953eb6cfaee
LautaroRomero21 <lautaroromerog021@gmail.com>
2024-07-15 11:58:13 -0300

    solicitudApertura funcionando

--- src/main/java/com/utndds/heladerasApi/models/Heladera/SolicitudApertura.java
+++ src/main/java/com/utndds/heladerasApi/models/Heladera/SolicitudApertura.java
@@ -1,10 +1,20 @@
 package com.utndds.heladerasApi.models.Heladera;
 
+import java.io.File;
+
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.utndds.heladerasApi.models.Rol.Colaborador;
 
 public class SolicitudApertura {
     Colaborador colaborador;
     Heladera heladera;
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
     public SolicitudApertura(Colaborador colaborador, Heladera heladera) {
         this.colaborador = colaborador;
@@ -19,15 +29,36 @@ public class SolicitudApertura {
     }
 
     public void iniciarTemporizador() {
-        System.out.println("temporizador iniciado para abrir: " + heladera.getNombre() + "por parte de "
+        System.out.println("Temporizador iniciado para abrir: " + heladera.getNombre() + " por parte de "
+                + colaborador.getPersona().getNombre());
+
+        int tiempo = this.tiempoLimite();
+
+        long delay = TimeUnit.HOURS.toSeconds(tiempo); // Convierte el tiempo en horas a segundos
+        scheduler.schedule(this::finalizarTemporizador, delay, TimeUnit.SECONDS);
+    }
+
+    private void finalizarTemporizador() {
+        this.heladera.eliminarSolicitud(this);
+        System.out.println("Temporizador finalizado para abrir: " + heladera.getNombre() + " por parte de "
                 + colaborador.getPersona().getNombre());
     }
 
-    public int leerTiempoDesdeJson(String filePath) {
-        return 3;
+    private int tiempoLimite() {
+        int limiteHoras = 0;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper
+                    .readTree(new File(".\\src\\main\\resources\\tiempoAperturaHeladera.json"));
+            limiteHoras = rootNode.get("limite_horas").asInt();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return limiteHoras;
     }
 
     public static void main(String[] args) {
+        int tiempo = new SolicitudApertura(null, null).tiempoLimite();
+        System.out.println(tiempo);
     }
-
 }
