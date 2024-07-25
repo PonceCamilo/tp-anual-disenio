505a67877c1797bfd53231206ce7d255a2be657f
LautaroRomero21 <lautaroromerog021@gmail.com>
2024-07-25 13:28:04 -0300

    carpeta mockAPI ordenada

--- src/main/java/com/utndds/mockAPI/MockApi.java
+++ src/main/java/com/utndds/mockAPI/MockApi.java
@@ -1,4 +1,4 @@
-package com.utndds.heladerasApi.mockAPI;
+package com.utndds.mockAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
