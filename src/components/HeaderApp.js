88ad664223b6289ba706342dcddb1450cd94cd99
Diego-Ing <dyanezromero@frba.utn.edu.ar>
2024-08-22 12:38:41 -0300

    Header App Responsive

--- src/components/HeaderApp.js
+++ src/components/HeaderApp.js
@@ -1,7 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../assets/styles/CustomContainer.css';
-import LoadCSVModal from './LoadCSVModal';
 import HeaderImage from '../assets/imgs/Header.png';
 function HeaderApp({ setHeaderHeight }) {
   const headerRef = useRef(null);
@@ -40,15 +39,15 @@ function HeaderApp({ setHeaderHeight }) {
         <p className='text-wrap fs-5 fw-normal text-break w-50'>
           Un espacio solidario para compartir alimentos con quienes más lo necesitan.
         </p>
-        <button className='btn btn-dark btn-lg mt-4 me-3'>Doná</button>
-        <button className='btn btn-dark btn-lg mt-4'>Sé Voluntario</button>
+        <div className='btn-group mt-4'>
+          <button className='btn btn-dark btn-lg me-3 flex-grow-1'>Doná</button>
+          <button className='btn btn-dark btn-lg flex-grow-1'>Sé Voluntario</button>
+        </div>
       </div>
 
       <div className='image-container'>
         <img src={HeaderImage} alt='Descripción' className='img-fluid' />
       </div>
-
-      
     </div>
   );
 }
