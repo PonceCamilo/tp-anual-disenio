c787f4f5ef0fe582bb997b45973aa9e953b9040a
LautaroRomero21 <lautaroromerog021@gmail.com>
2024-08-29 21:37:22 -0300

    donar dinero page mas presentable

--- src/components/DonacionDineroForm.js
+++ src/components/DonacionDineroForm.js
@@ -1,61 +1,37 @@
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import MercadoPago from '../assets/iconos/MercadoPago.svg';
-import PayPal from '../assets/iconos/PayPal.svg';
-import CreditCard from '../assets/iconos/CreditCar.svg';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import MercadoPago from "../assets/iconos/MercadoPago.svg";
+import PayPal from "../assets/iconos/PayPal.svg";
+import CreditCard from "../assets/iconos/CreditCard.svg"; // Fixed typo from "CreditCar.svg"
+import "../assets/styles/DonacionDineroForm.css";
+
 function DonacionDineroForm() {
   return (
-    <Form className=' d-flex flex-column justify-content-between'>
-      <h1 className='mb-5 text-center'>Donación de dinero</h1>
-      <Form.Group controlId="monto">
+    <Form className="donacion-dinero-form">
+      <h1 className="form-title text-center">Donación de dinero</h1>
+
+      <Form.Group controlId="monto" className="form-group">
         <Form.Label>Monto:</Form.Label>
-        <Form.Control type="number" name="monto" placeholder="Ingrese el monto a donar" />
+        <Form.Control
+          type="number"
+          name="monto"
+          placeholder="Ingrese el monto a donar"
+          className="form-control"
+        />
       </Form.Group>
 
-      <h3 className='mt-4'>Doná con los siguientes métodos de pago:</h3>
-      <div className="mt-3 d-flex justify-content-around">
-        <img src={MercadoPago} alt="Mercado Pago" />
-        <img src={PayPal} alt="PayPal" />
-        <img src={CreditCard} alt="Tarjeta de Credito" />
+      <h3 className="payment-methods-title">Doná con los siguientes métodos de pago:</h3>
+      <div className="payment-methods d-flex justify-content-around">
+        <img src={MercadoPago} alt="Mercado Pago" className="payment-icon" />
+        <img src={PayPal} alt="PayPal" className="payment-icon" />
+        <img src={CreditCard} alt="Tarjeta de Crédito" className="payment-icon" />
       </div>
 
-
-      {/* 
-      
-       Confirmation Modal 
-      <Modal show={showModal} onHide={handleModalClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Confirmación de Donación</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>Estás a punto de donar {donationAmount} {currency}.</p>
-          <p>¿Deseas continuar?</p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleModalClose}>
-            Cancelar
-          </Button>
-          <Button variant="primary" onClick={handleModalClose}>
-            Confirmar Donación
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </Form>
-      
-      ---------- Progress Bar-------------
-     <div className="mt-4">
-        <ProgressBar now={60} label={`$600 recaudados`} />
-      </div>
-
-      <Button variant="primary" type="submit" className="mt-4">Donar</Button>
-    </Form>
-    */ }
-
-
-<button className='btn btn-outline-dark btn-lg mt-4'>Donar</button>
+      <Button variant="primary" type="submit" className="submit-button btn-lg">
+        Donar
+      </Button>
     </Form>
   );
 }
 
 export default DonacionDineroForm;
-
--- src/pages/DonacionDineroPage.js
+++ src/pages/DonacionDineroPage.js
@@ -1,11 +1,22 @@
+// DonacionDineroPage.js
 import DonacionDineroForm from "../components/DonacionDineroForm";
 import "../assets/styles/DonacionDineroPage.css";
 
 function DonacionDineroPage() {
   return (
-    <div className="donacion-dinero-page d-flex justify-content-center align-items-center">
-      <DonacionDineroForm />
+    <div className="donacion-dinero-page">
+      <div className="left-side">
+        <h1>Donar Dinero</h1>
+        <p>
+          Con tu ayuda, podemos marcar una diferencia. Cualquier cantidad que
+          puedas donar es invaluable para nuestro esfuerzo continuo.
+        </p>
+      </div>
+      <div className="right-side">
+        <DonacionDineroForm />
+      </div>
     </div>
   );
 }
+
 export default DonacionDineroPage;
