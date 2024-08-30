074effe4af38e174b5e30b6f2a382c7fb303947d
LautaroRomero21 <lautaroromerog021@gmail.com>
2024-08-30 15:15:57 -0300

    responnsive nav arreglado

--- src/components/NavApp.js
+++ src/components/NavApp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -7,19 +7,44 @@ import UTNlogo from '../assets/logos/utn.svg';
 import IcoProfile from '../assets/iconos/IcoProfile.svg';
 import IcoHeladera from '../assets/iconos/IcoHeladera.svg';
 import LoginModal from './LoginModal';
-import '../assets/styles/CustomContainer.css'; 
+import '../assets/styles/CustomContainer.css';
 
 function NavApp({ className }) {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
   const handleLoginClick = () => {
     setShowModal(true);
   };
 
+  const handleResize = () => {
+    if (window.innerWidth >= 992 && expanded) {
+      setExpanded(false);
+    }
+  };
+
+  const handleNavLinkClick = () => {
+    setExpanded(false); // Colapsa el Navbar al hacer clic en un enlace de navegación
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [expanded]);
+
   return (
     <>
-      <Navbar className={`Nav-Bar ${className}`} expand="lg" fixed="top">
+      <Navbar
+        className={`Nav-Bar ${className}`}
+        expand="lg"
+        fixed="top"
+        expanded={expanded}
+        onToggle={() => setExpanded(!expanded)}
+        style={{ zIndex: 1050 }} /* En caso de que prefieras aplicar inline style */
+      >
         <Container>
           <Navbar.Brand as={Link} to="/" className="fs-3 d-flex align-items-center">
             <img
@@ -35,7 +60,7 @@ function NavApp({ className }) {
           <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
             <Nav>
               {isAuthenticated ? (
-                <Nav.Link href="#profile" className="fs-5">
+                <Nav.Link href="#profile" onClick={handleNavLinkClick} className="fs-5">
                   <img
                     alt=""
                     src={IcoProfile}
@@ -57,7 +82,7 @@ function NavApp({ className }) {
                   {' Ingresar'}
                 </Nav.Link>
               )}
-              <Nav.Link as={Link} to="/map" className="fs-5">
+              <Nav.Link as={Link} to="/map" onClick={handleNavLinkClick} className="fs-5">
                 <img
                   alt=""
                   src={IcoHeladera}
