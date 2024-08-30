bed9d8091f419f37557fac45878ae4a09a19cafa
Franco Stazzone <fralstazzone@gmail.com>
2024-08-30 12:08:22 -0300

    Reportes

--- src/pages/ReportesPage.js
+++ src/pages/ReportesPage.js
@@ -2,94 +2,94 @@ import React, { useState } from 'react';
 import { Button, Collapse } from 'react-bootstrap';
 import "../assets/styles/Reportes.css"
 
+const BotonReporte = ({ texto, section, handleClick, openSection }) => (
+    <Button
+        className="mt-1"
+        onClick={() => handleClick(section)}
+        aria-controls={`collapse-${section}`}
+        aria-expanded={openSection === section}>
+        {texto}
+    </Button>
+);
+
+const SeccionReporte = ({ clase, columnas, data, openSection }) => (
+    <Collapse in={openSection === clase}>
+        <div id={`collapse-${clase}`} className='collapse-content'>
+            <div className={`${clase}`}> 
+                {columnas.map((columna) => (
+                    <p>{columna}</p>
+                ))}
+            </div>
+            {data.map((elemento) => (
+                <div className={`${clase}`}>
+                    {Object.values(elemento).map((value) => (
+                        <p>{value}</p>
+                    ))}
+                </div>
+            ))}
+        </div>
+    </Collapse>
+);
+
 function ReportesPage() {
     const [openSection, setOpenSection] = useState(null);
-    {/*const [listaReporte, setListaReporte] = useState([]);*/}
+    const [reportes, setReportes] = useState([]);
 
-    {/* TEST */}
-    const listaReporte = [
-        {
-            nombreHeladera: "Heladera de Buenos Aires",
-            fallas: 4
-        },
-        {
-            nombreHeladera: "Heladera de Córdoba",
-            fallas: 2
-        },
-        {
-            nombreHeladera: "Heladera de Mendoza",
-            fallas: 3
-        },
-        {
-            nombreHeladera: "Heladera de Rosario",
-            fallas: 1
-        },
-        {
-            nombreHeladera: "Heladera de Salta",
-            fallas: 5
-        }
-    ];
-    {/* TEST */}
 
-    const toggleSection = (section) => {
-        setOpenSection(prevSection => (prevSection === section ? null : section));
-    };
+    {/* Data para probar */}
+    const mockData = {
+            "viandas-heladeras": [
+                { nombreHeladera: "Heladera de Buenos Aires", viandasColocadas: 5, viandasRetiradas: 3 },
+                { nombreHeladera: "Heladera de Córdoba", viandasColocadas: 7, viandasRetiradas: 6 },
+                { nombreHeladera: "Heladera de Mendoza", viandasColocadas: 4, viandasRetiradas: 4 },
+            ],
+            "fallas": [
+                { nombreHeladera: "Heladera de Buenos Aires", fallas: 2 },
+                { nombreHeladera: "Heladera de Córdoba", fallas: 1 },
+                { nombreHeladera: "Heladera de Mendoza", fallas: 3 },
+            ],
+            "viandas-colaboradores": [
+                { nombreColaborador: "Juan Pérez", viandasDonadas: 10 },
+                { nombreColaborador: "María López", viandasDonadas: 8 },
+                { nombreColaborador: "Carlos Gómez", viandasDonadas: 15 },
+            ]
+        };
+
+
+    const handleButtonClick = (section) => {
+        // Falta implementar la llamada a la API
+        setOpenSection(prevSection => (prevSection === section ? null : section));      
+    }
 
     return (
         <div className="reportes-page">
             <div className='reportes-container'>
-            <div className="botones">
-                    <Button className="mt-1"
-                        onClick={() => toggleSection('fallas')}
-                        aria-controls="collapse-fallas"
-                        aria-expanded={openSection === 'fallas'}>
-                        Fallas por heladera
-                    </Button>
-
-                    <Button
-                        className="mt-1"
-                        onClick={() => toggleSection('viandasHeladera')}
-                        aria-controls="collapse-viandas-heladera"
-                        aria-expanded={openSection === 'viandasHeladera'}>
-                        Viandas por heladera
-                    </Button>
-                    
-                    <Button
-                        className="mt-1"
-                        onClick={() => toggleSection('viandasColaborador')}
-                        aria-controls="collapse-viandas-colaborador"
-                        aria-expanded={openSection === 'viandasColaborador'}>
-                        Viandas por colaborador
-                    </Button>
+                <div className="botones">
+                    <BotonReporte texto="Fallas por heladera" section="fallas" handleClick={handleButtonClick} openSection={openSection} />
+                    <BotonReporte texto="Viandas por heladera" section="viandas-heladeras" handleClick={handleButtonClick} openSection={openSection} />
+                    <BotonReporte texto="Viandas por colaborador" section="viandas-colaboradores" handleClick={handleButtonClick} openSection={openSection} />
                 </div>
 
-                <Collapse in={openSection === 'fallas'}>
-                    <div id="collapse-fallas">
-                        <div className='fallas'> 
-                            <p>Heladera</p>
-                            <p>Fallas</p>
-                        </div>
-                        {listaReporte.map((heladera, index) => (
-                            <div className="fallas" key={index}>
-                                <p>{heladera.nombreHeladera}</p>
-                                <p>{heladera.fallas}</p>
-                            </div>
-                        ))}
-                    </div>
-                </Collapse>
+                <SeccionReporte 
+                    clase="fallas" 
+                    columnas={['Heladera', 'Fallas']} 
+                    data={mockData['fallas']} 
+                    openSection={openSection} 
+                />
 
-                <Collapse in={openSection === 'viandasHeladera'}>
-                    <div id="collapse-viandas-heladera">
-                        Contenido para Viandas por heladera
-                    </div>
-                </Collapse>
+                <SeccionReporte 
+                    clase="viandas-heladeras" 
+                    columnas={['Heladera', 'Viandas retiradas', 'Viandas colocadas']} 
+                    data={mockData['viandas-heladeras']} 
+                    openSection={openSection} 
+                />
 
-                <Collapse in={openSection === 'viandasColaborador'}>
-                    <div id="collapse-viandas-colaborador">
-                        Contenido para Viandas por colaborador
-                    </div>
-                </Collapse>
-
+                <SeccionReporte 
+                    clase="viandas-colaboradores"
+                    columnas={['Colaborador', 'Viandas donadas']} 
+                    data={mockData['viandas-colaboradores']} 
+                    openSection={openSection} 
+                />
             </div>
         </div>
     );
