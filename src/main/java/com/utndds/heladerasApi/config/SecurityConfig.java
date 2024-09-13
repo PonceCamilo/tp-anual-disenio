0c0120ef16d665a79034fbdae05abec44f814c1a
Camilo Ponce <camiloponce04@gmail.com>
2024-09-13 15:12:46 -0300

    arreglado bug google maps (securityConfig.java)

--- src/main/java/com/utndds/heladerasApi/config/SecurityConfig.java
+++ src/main/java/com/utndds/heladerasApi/config/SecurityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
+import org.springframework.http.HttpMethod;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 import java.io.IOException;
@@ -31,10 +31,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
-            .cors().configurationSource(corsConfigurationSource()) // Agrega la configuración CORS aquí
-            .and()
+            .csrf((csrf)->csrf.disable())
+            .cors(withDefaults())
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/", "/images/**").permitAll()
+                .requestMatchers(HttpMethod.GET,"/ubicaciones-googlemaps").permitAll()
                 .anyRequest().authenticated())
             .oauth2Login(withDefaults())
             .logout(logout -> logout
