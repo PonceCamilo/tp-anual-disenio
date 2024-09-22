04c58b9b13160b2f3cfadf2e25c3773cd21d5d42
LautaroRomero21 <lautaroromerog021@gmail.com>
2024-09-22 16:37:29 -0300

    service 2 agregado

---
+++ src/main/java/com/utndds/heladerasApi/services/ConocerColaboracionesService.java
@@ -0,0 +1,28 @@
+package com.utndds.heladerasApi.services;
+
+import com.utndds.heladerasApi.models.Colaboraciones.Colaboracion;
+import com.utndds.heladerasApi.models.Colaboraciones.DonacionVianda;
+import com.utndds.heladerasApi.models.Rol.Colaborador;
+import java.time.LocalDate;
+import java.util.List;
+
+public class ConocerColaboracionesService {
+
+    public int getViandasDonadas(Colaborador colaborador, int dias) {
+        int viandasDonadas = 0;
+        LocalDate fechaInicio = LocalDate.now().minusDays(dias); // Define el inicio del rango de tiempo
+
+        List<Colaboracion> colaboraciones = colaborador.getColaboraciones();
+        for (Colaboracion colaboracion : colaboraciones) {
+            if (colaboracion instanceof DonacionVianda) {
+                DonacionVianda donacion = (DonacionVianda) colaboracion;
+                if (donacion.getFecha().isAfter(fechaInicio)) {
+                    viandasDonadas++;
+                }
+            }
+        }
+        return viandasDonadas;
+    }
+
+    
+}
